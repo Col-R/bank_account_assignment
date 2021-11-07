e9c57c95f18cb38bfdd0040dd797f9f3fde2ebae
Col-R <2colewestin@gmail.com>
2021-11-07 16:39:42 -0600

    added some test cases

--- BankAccountTest.java
+++ BankAccountTest.java
@@ -1,8 +1,13 @@
 public class BankAccountTest {
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount(100,250);
-        BankAccount account2 = new BankAccount(120,75);
+        BankAccount acc1 = new BankAccount();
+        acc1.deposit(400, "checking");
+        acc1.withdraw(120, "checking");
+        System.out.println(acc1.getChecking());
+        BankAccount acc2 = new BankAccount();
+        acc2.deposit(500, "savings");
+        acc2.deposit(120, "checking");
+        System.out.println(acc2.totalCash());
         System.out.println(BankAccount.numberOfAccounts);
-        
     }
 }
